8ad6c16ff35e8d2b24310aacd56eda1327eec2c0
Prime_N2 <nikitaprime83@gmail.com>
2021-01-07 12:37:55 +0400

    Fixed buggs

---
+++ Assets/Scripts/FrameRate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FrameRate : MonoBehaviour
+{
+    [SerializeField] private int _frameRate = 90;
+
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+    }
+
+    void Update()
+    {
+        if (_frameRate != Application.targetFrameRate)
+            Application.targetFrameRate = _frameRate;
+
+    }
+}
+
+
